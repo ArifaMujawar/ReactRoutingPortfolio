e542bf4c1e0a30a0d3c600cf4102aeee3ca5e1c8
Arifa Mujawar <arifabegam.mujawar@gmail.com>
2018-02-21 13:23:32 +0200

    Indecision App Stage 1 completed

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -4,91 +4,90 @@ console.log('App.js is running');
 var app = {
     title: 'Indecision App',
     subtitle: 'Put your life in the hands of a computer',
-    items: ['one', 'two']
-};
-var template = React.createElement(
-    'div',
-    null,
-    React.createElement(
-        'h1',
-        null,
-        app.title,
-        ' '
-    ),
-    app.subtitle && React.createElement(
-        'p',
-        null,
-        app.subtitle
-    ),
-    React.createElement(
-        'p',
-        null,
-        app.items.length > 0 ? "Here are your options" : "No Options"
-    ),
-    React.createElement(
-        'ol',
-        null,
-        React.createElement(
-            'li',
-            null,
-            ' ',
-            app.items[0]
-        ),
-        React.createElement(
-            'li',
-            null,
-            ' ',
-            app.items[1],
-            ' '
-        )
-    )
-);
-var count = 0;
-var addOne = function addOne() {
-    count++;
-    renderCounterApp();
-    console.log('addOne');
-};
-var minusOne = function minusOne() {
-    count--;
-    renderCounterApp();
-    console.log('minusOne');
-};
-var reset = function reset() {
-    count = 0;
-    renderCounterApp();
-    console.log('reset');
+    options: []
 };
 
-var appRoot = document.getElementById('app');
+var onFormSubmit = function onFormSubmit(e) {
+    // e contains info about event
+    e.preventDefault(); // prevents full page refresh, instead below code is exec
 
-var renderCounterApp = function renderCounterApp() {
-    var templateTwo = React.createElement(
+    var option = e.target.elements.option.value;
+    // e.target points to element that event started on,option is value of form name attribute.
+
+    if (option) {
+        app.options.push(option);
+        e.target.elements.option.value = '';
+    }
+    RenderList();
+};
+
+var onRemoveAll = function onRemoveAll() {
+    app.options = [];
+    RenderList();
+};
+
+var onMakeDecision = function onMakeDecision() {
+    var randomNum = Math.floor(Math.random() * app.options.length);
+    var option = app.options[randomNum];
+    alert(option);
+};
+
+var RenderList = function RenderList() {
+    var template = React.createElement(
         'div',
         null,
         React.createElement(
             'h1',
             null,
-            'Count: ',
-            count
+            app.title,
+            ' '
+        ),
+        app.subtitle && React.createElement(
+            'p',
+            null,
+            app.subtitle
+        ),
+        React.createElement(
+            'p',
+            null,
+            app.options.length > 0 ? "Here are your options" : "No Options"
         ),
         React.createElement(
             'button',
-            { onClick: addOne },
-            ' +1 '
+            { disabled: app.options.length === 0, onClick: onMakeDecision },
+            'What should I do?'
         ),
         React.createElement(
             'button',
-            { onClick: minusOne },
-            ' -1 '
+            { onClick: onRemoveAll },
+            'Remove All'
         ),
         React.createElement(
-            'button',
-            { onClick: reset },
-            ' reset '
+            'ol',
+            null,
+            app.options.map(function (item) {
+                return React.createElement(
+                    'li',
+                    { key: item },
+                    ' Item: ',
+                    item
+                );
+            })
+        ),
+        React.createElement(
+            'form',
+            { onSubmit: onFormSubmit },
+            React.createElement('input', { type: 'text', name: 'option' }),
+            React.createElement(
+                'button',
+                null,
+                ' Add Option '
+            )
         )
     );
-    ReactDOM.render(templateTwo, appRoot);
+    var appRoot = document.getElementById('app');
+
+    ReactDOM.render(template, appRoot);
 };
 
-renderCounterApp();
+RenderList();
--- src/app.js
+++ src/app.js
@@ -2,54 +2,62 @@ console.log('App.js is running');
 const app= {
     title:'Indecision App',
     subtitle: 'Put your life in the hands of a computer',
-    items:['one','two']
+    options:[]
 };
-const template = (
-<div> 
-    <h1>{app.title} </h1>
+
+const onFormSubmit = (e) => {// e contains info about event
+    e.preventDefault();// prevents full page refresh, instead below code is exec
+
+    const option = e.target.elements.option.value;
+    // e.target points to element that event started on,option is value of form name attribute.
+
+    if(option){
+        app.options.push(option);
+        e.target.elements.option.value = '';
+    }
+    RenderList();
+};
+
+const onRemoveAll = () =>{
+    app.options = [];
+    RenderList();
+};
+
+const onMakeDecision = () => {
+    const randomNum = Math.floor(Math.random() * app.options.length);
+    const option = app.options[randomNum];
+    alert(option);
+};
+
+const RenderList = () =>{
+    const template = (
+        <div> 
+            <h1>{app.title} </h1>
+        
+            {app.subtitle && <p>{app.subtitle }</p>}
+            <p>{( app.options.length >0) ? "Here are your options" : "No Options"}</p>
     
-    {app.subtitle && <p>{app.subtitle }</p>}
-<p>
-{( app.items.length >0) ? "Here are your options" : "No Options"}</p>
-
-<ol><li> {app.items[0]}</li>
-<li> {app.items[1]} </li>
-</ol>
-
-</div>
-);
-let count = 0;
-const addOne = () => {
-    count++;
-    renderCounterApp();
-    console.log('addOne');
-};
-const minusOne = () => {
-    count--;
-    renderCounterApp();
-    console.log('minusOne');
-};
-const reset = () => {
-    count =0;
-    renderCounterApp();
-    console.log('reset');
-};
-
-const appRoot = document.getElementById('app');
-
-const renderCounterApp = () => {
-    const templateTwo = (
-        <div>
-            <h1>Count: {count}</h1>
-            <button onClick = {addOne}> +1 </button>
-            <button onClick = {minusOne}> -1 </button>
-            <button onClick = {reset}> reset </button>
-        </div>
+            <button disabled={app.options.length === 0} onClick={onMakeDecision}>What should I do?</button>
+            <button onClick={onRemoveAll}>Remove All</button>
+            <ol>
+               {    
+                   app.options.map((item) => <li key ={item}> Item: {item}</li>)
+                    
+               }
+       
+            </ol>
+    
+        <form onSubmit={onFormSubmit}>
+        {/* onSubmit equals to funct, that handles event .Also,here we just reference function name and do not call.*/}
+            <input type="text" name="option" />
+            <button> Add Option </button>
+        </form>
+    </div>
     );
-    ReactDOM.render(templateTwo,appRoot );
+    const appRoot = document.getElementById('app');
+    
+    
+    ReactDOM.render(template, appRoot);
 };
 
-renderCounterApp();
-
-
-
+RenderList();
