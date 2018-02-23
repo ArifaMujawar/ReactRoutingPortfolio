afee0083904590efe0f3a017f23b48404f127860
Arifa Mujawar <arifabegam.mujawar@gmail.com>
2018-02-23 11:33:54 +0200

    Learnt about using ES6 classes and sub-classes

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,39 +1,75 @@
 'use strict';
 
-var visibility = false;
+var _get = function get(object, property, receiver) { if (object === null) object = Function.prototype; var desc = Object.getOwnPropertyDescriptor(object, property); if (desc === undefined) { var parent = Object.getPrototypeOf(object); if (parent === null) { return undefined; } else { return get(parent, property, receiver); } } else if ("value" in desc) { return desc.value; } else { var getter = desc.get; if (getter === undefined) { return undefined; } return getter.call(receiver); } };
 
-var toggleContent = function toggleContent() {
-    visibility = !visibility;
-    renderFunc();
-};
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
 
-var renderFunc = function renderFunc() {
-    var template = React.createElement(
-        'div',
-        null,
-        React.createElement(
-            'h1',
-            null,
-            'Visibility Toggle'
-        ),
-        React.createElement(
-            'button',
-            { onClick: toggleContent },
-            visibility ? 'Hide Details' : 'Show Details'
-        ),
-        visibility && React.createElement(
-            'div',
-            null,
-            React.createElement(
-                'p',
-                null,
-                'Hey ! these are some details! '
-            )
-        )
-    );
+function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
 
-    var appRoot = document.getElementById('app');
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
 
-    ReactDOM.render(template, appRoot);
-};
-renderFunc();
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+var Person = function () {
+    function Person() {
+        var name = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : 'Anonymous';
+        var age = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : 0;
+
+        _classCallCheck(this, Person);
+
+        this.name = name;
+        this.age = age;
+    }
+
+    _createClass(Person, [{
+        key: 'getDescription',
+        value: function getDescription() {
+            return this.name + ' is ' + this.age + ' year(s) old.';
+        }
+    }, {
+        key: 'getGreeting',
+        value: function getGreeting() {
+            return 'Hello, I\'m ' + this.name + '.';
+        }
+    }]);
+
+    return Person;
+}();
+
+var Traveler = function (_Person) {
+    _inherits(Traveler, _Person);
+
+    function Traveler(name, age, location) {
+        _classCallCheck(this, Traveler);
+
+        var _this = _possibleConstructorReturn(this, (Traveler.__proto__ || Object.getPrototypeOf(Traveler)).call(this, name, age));
+
+        _this.location = location;
+        return _this;
+    }
+
+    _createClass(Traveler, [{
+        key: 'hasLocation',
+        value: function hasLocation() {
+            return !!this.location;
+        }
+    }, {
+        key: 'getGreeting',
+        value: function getGreeting() {
+            var greeting = _get(Traveler.prototype.__proto__ || Object.getPrototypeOf(Traveler.prototype), 'getGreeting', this).call(this);
+
+            if (this.hasLocation()) {
+                greeting += 'I\'m visiting from ' + this.location + '.';
+            }
+            return greeting;
+        }
+    }]);
+
+    return Traveler;
+}(Person);
+
+var me = new Traveler('Arifa Mujawar', 25, 'bgm');
+console.log(me.getGreeting());
+
+var you = new Traveler('sas');
+console.log(you.getGreeting());
